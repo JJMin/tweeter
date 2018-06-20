f023061d6087f88d5eae20eaba88998a7041d93e
Eric Joo <ericjaeminjoo@gmail.com>
2018-06-20 13:23:55 +0000

    Add form validation to check if, on tweet submission, user left tweet empty or tweet is too long

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -46,23 +46,42 @@ $(document).ready(function () {
   function loadTweets() {
     console.log('Button clicked, performing ajax call...');
     $.ajax({
-      url: '/tweets',
-      method: 'GET'
+        url: '/tweets',
+        method: 'GET'
       })
-        .done((tweets) => {
-          renderTweets(tweets);
-        })
-        .fail((error) => {
-          console.log("Error!");
-        })
-        .always(() => {
-          console.log("Function complete.");
-    });
+      .done((tweets) => {
+        renderTweets(tweets);
+      })
+      .fail((error) => {
+        console.log("Error!");
+      })
+      .always(() => {
+        console.log("Function complete.");
+      });
   };
 
-  $(".new_tweet form").submit(function (event) {
+  $("#new_tweet").keyup(function () {
+    let tweetLength = $(this).val().length;
+    let characterNumber = $('#character-count').text();
+
+    if (tweetLength > 140) {
+      $('#character-count').css('color', 'red');
+    } else {
+      $('#character-count').css('color', 'black');
+    }
+
+    $('#character-count').text(140 - tweetLength);
+  });
+
+  $(".new_tweet_container form").submit(function (event) {
+    if ($("#new_tweet").val().length === 0) {
+      alert("You cannot tweet an empty message!");
+    } else if ($("#new_tweet").val().length > 140) {
+      alert("You cannot tweet a message that has more than 140 character!");
+    } else {
+      $(this).serialize();
+    }
     event.preventDefault();
-    $(this).serialize();
   });
 
   loadTweets();
